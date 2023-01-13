2e97a8aad330fdc022ed3e93d511575b32eed703
Kiran <kirankumarbs1034@gmail.com>
2023-01-13 21:12:20 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCore_AppConfigurationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
