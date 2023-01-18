160bfc22cc847101e966436d5d8646200c60509a
Kiran <kirankumarbs1034@gmail.com>
2023-01-18 20:03:29 +0530

    Added 1st way of reading config data.

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -4,10 +4,29 @@ namespace DotNetCore_AppConfigurationDemo.Controllers
 {
     public class TestController : Controller
     {
-       
-        public IActionResult Index()
+       //Adding dependency injection
+        
+        private readonly IConfiguration _configuration;
+        public TestController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration; //To access the config data from app settings.json
+        }
+        public string Index()
+        {
+            //Reading the configuration data from appsetings.json using _configuration instance
+            var myKeyValue = _configuration["MyKey"];
+            var title = _configuration["Position:Title"];
+            var name = _configuration["Position:Name"];
+            var defaultLogLevel = _configuration["Logging:LogLevel:Default"];
+
+            string reult = $"myKeyValue : {myKeyValue}, title : {title}, name : {name}, defaultLogLevel : {defaultLogLevel}";
+            
+            return reult;
         }
     }
 }
+
+/*
+
+
+ */
