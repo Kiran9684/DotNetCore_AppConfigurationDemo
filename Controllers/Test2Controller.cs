429356211d4efec2b62ee87710cfd35f4d5b9f2d
Kiran <kirankumarbs1034@gmail.com>
2023-01-18 22:06:05 +0530

    Added test 2 controller for demonstrating 3rd approach to read config data

---
+++ Controllers/Test2Controller.cs
@@ -0,0 +1,22 @@
+﻿using DotNetCore_AppConfigurationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DotNetCore_AppConfigurationDemo.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        private readonly PositionOptions _positionOptioins;
+
+        public Test2Controller(IOptions<PositionOptions> options) 
+        {
+            _positionOptioins = options.Value;
+        }
+
+        public string Index()
+        {
+
+            return  $"{_positionOptioins.Name} and {_positionOptioins.Title}";
+        }
+    }
+}
