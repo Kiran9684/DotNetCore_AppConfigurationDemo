7306c8bbfb5e871ae90ed0a424b065f311429a6a
Kiran <kirankumarbs1034@gmail.com>
2023-01-20 00:27:11 +0530

    Added test 3 controller, to read  config data bro dbconfigdata config json file with IOptions approach

---
+++ Controllers/Test3Controller.cs
@@ -0,0 +1,23 @@
+﻿using DotNetCore_AppConfigurationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DotNetCore_AppConfigurationDemo.Controllers
+{
+    public class Test3Controller : Controller
+    {
+        public readonly DbConfigData _dbConfigData;
+
+        public Test3Controller(IOptions<DbConfigData> options)
+        {
+            //Biding config data using IOptions approach. 
+            _dbConfigData = options.Value;
+        }
+        public string Index()
+        {
+            var data1 = _dbConfigData.connectionstrings;
+            var data2 = _dbConfigData.storedprocs;
+            return "Test";
+        }
+    }
+}
