2d8cf9ebf84fbb6a987869bea9dba3965f53737a
Kiran <kirankumarbs1034@gmail.com>
2023-01-20 00:25:29 +0530

    Added template class for dbConfigdata to bind

---
+++ Models/DbConfigData.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCore_AppConfigurationDemo.Models
+{
+    public class DbConfigData
+    {
+        public Dictionary<string, string> connectionstrings { get;set; }
+        public Dictionary<string, string> storedprocs { get;set; }
+    }
+}
